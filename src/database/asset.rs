21de4c1658cac45ef1261717b288f05784340e3d
shigedangao <marc.inthaamnouay@gmail.com>
2023-03-27 12:33:12 +0200

    fix sqlx query

--- src/database/asset.rs
+++ src/database/asset.rs
@@ -3,7 +3,9 @@ use std::collections::HashMap;
 
 #[derive(Debug, sqlx::FromRow)]
 pub struct Assets {
-    id: i64,
+    #[sqlx(rename = "Id")]
+    id: i32,
+    #[sqlx(rename = "Code")]
     code: String,
 }
 
@@ -16,11 +18,11 @@ impl Assets {
     pub async fn get_assets(
         handler: &Handler,
     ) -> Result<HashMap<String, i64>, Box<dyn std::error::Error>> {
-        let assets = sqlx::query_as::<_, Assets>("SELECT Id, Code FROM Assets")
+        let assets = sqlx::query_as::<_, Assets>(r#"SELECT "Id", "Code" FROM "Assets""#)
             .fetch_all(&handler.pool)
             .await?;
 
-        let assets_map: HashMap<String, i64> = assets.into_iter().map(|a| (a.code, a.id)).collect();
+        let assets_map: HashMap<String, i64> = assets.into_iter().map(|a| (a.code, a.id as i64)).collect();
 
         Ok(assets_map)
     }
